daa0581ce53897bc949d73daf5262a966e18eaca
josephb <josephb@E1118W05>
2020-02-04 14:39:52 -0600

    Sprites created, ball moving, player moves.
     To do: detect collision

--- MonoGameWindowsStarter/Game1.cs
+++ MonoGameWindowsStarter/Game1.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
+using System;
 
 namespace MonoGameWindowsStarter
 {
@@ -11,6 +12,13 @@ namespace MonoGameWindowsStarter
     {
         GraphicsDeviceManager graphics;
         SpriteBatch spriteBatch;
+        Random random = new Random();
+        Texture2D ball;
+        Rectangle ballRect;
+        Vector2 ballPosition;
+        Vector2 ballVelocity;
+        Texture2D player;
+        Rectangle playerRect;
 
         public Game1()
         {
@@ -27,6 +35,26 @@ namespace MonoGameWindowsStarter
         protected override void Initialize()
         {
             // TODO: Add your initialization logic here
+            graphics.PreferredBackBufferWidth = 1500;
+            graphics.PreferredBackBufferHeight = 900;
+            graphics.ApplyChanges();
+
+            //ball physics initialization
+            ballPosition = Vector2.Zero;
+            ballVelocity = new Vector2((float)random.NextDouble(),(float)random.NextDouble());
+            ballVelocity.Normalize();
+
+            //ball bounding rectangle initialization
+            ballRect.X = 0;
+            ballRect.Y = 0;
+            ballRect.Width = 100;
+            ballRect.Height = 100;
+
+            //player sprite location initialization
+            playerRect.Width = 100;
+            playerRect.Height = 150;
+            playerRect.X = (graphics.PreferredBackBufferWidth / 2);
+            playerRect.Y = graphics.PreferredBackBufferHeight - playerRect.Height;
 
             base.Initialize();
         }
@@ -41,6 +69,8 @@ namespace MonoGameWindowsStarter
             spriteBatch = new SpriteBatch(GraphicsDevice);
 
             // TODO: use this.Content to load your game content here
+            ball = Content.Load<Texture2D>("ball");
+            player = Content.Load<Texture2D>("player");
         }
 
         /// <summary>
@@ -62,7 +92,84 @@ namespace MonoGameWindowsStarter
             if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
                 Exit();
 
-            // TODO: Add your update logic here
+            //update ball position
+            ballPosition += (float)gameTime.ElapsedGameTime.TotalMilliseconds * ballVelocity;
+
+            //player controls
+            if (Keyboard.GetState().IsKeyDown(Keys.Left))
+            {
+                playerRect.X -= 15;
+            }
+            if (Keyboard.GetState().IsKeyDown(Keys.Right))
+            {
+                playerRect.X += 15;
+            }
+
+            //check for player wall collision
+            if(playerRect.X < 0)
+            {
+                playerRect.X = 0;
+            }
+            if (playerRect.X > (GraphicsDevice.Viewport.Width - playerRect.Width))
+            {
+                playerRect.X = (GraphicsDevice.Viewport.Width - playerRect.Width);
+            }
+
+
+            //check for ball wall collision
+            if (ballPosition.Y < 0)
+            {
+                ballVelocity.Y *= -1;
+                float delta = 0 - ballPosition.Y;
+                ballPosition.Y += 2 * delta;
+            }
+            if (ballPosition.Y > graphics.PreferredBackBufferHeight - 100)
+            {
+                ballVelocity.Y *= -1;
+                float delta = graphics.PreferredBackBufferHeight - 100 - ballPosition.Y;
+                ballPosition.Y += 2 * delta;
+            }
+            if (ballPosition.X < 0)
+            {
+                ballVelocity.X *= -1;
+                float delta = 0 - ballPosition.X;
+                ballPosition.X += 2 * delta;
+            }
+            if (ballPosition.X > graphics.PreferredBackBufferWidth - 100)
+            {
+                ballVelocity.X *= -1;
+                float delta = graphics.PreferredBackBufferWidth - 100 - ballPosition.X;
+                ballPosition.X += 2 * delta;
+            }
+
+            ballRect.X = (int)ballPosition.X;
+            ballRect.Y = (int)ballPosition.Y;
+
+            //check for ball collision with player
+            if (ballPosition.X == playerRect.X + playerRect.Width)
+            {
+                ballVelocity.X *= -1;
+                float delta = 0 - ballPosition.X;
+                ballPosition.X += 2 * delta;
+            }
+            if (ballPosition.X + ballRect.Width < playerRect.X)
+            {
+                ballVelocity.X *= -1;
+                float delta = 0 - ballPosition.X;
+                ballPosition.X += 2 * delta;
+            }
+            if (ballPosition.Y < playerRect.Y + playerRect.Height)
+            {
+                ballVelocity.Y *= -1;
+                float delta = 0 - ballPosition.Y;
+                ballPosition.Y += 2 * delta;
+            }
+            if (ballPosition.Y + ballRect.Height > playerRect.Y)
+            {
+                ballVelocity.Y *= -1;
+                float delta = 0 - ballPosition.Y;
+                ballPosition.Y += 2 * delta;
+            }
 
             base.Update(gameTime);
         }
@@ -76,6 +183,10 @@ namespace MonoGameWindowsStarter
             GraphicsDevice.Clear(Color.CornflowerBlue);
 
             // TODO: Add your drawing code here
+            spriteBatch.Begin();
+            spriteBatch.Draw(ball, ballRect, Color.White);
+            spriteBatch.Draw(player, playerRect, Color.White);
+            spriteBatch.End();
 
             base.Draw(gameTime);
         }
